e3f3ff7859159d26004b5bbb5fbec334d932a4eb
m_aykurt <maykurt92@gmail.com>
2021-08-05 08:29:51 +0300

    com-1

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,9 +1,23 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
+const ps = document.querySelectorAll("h1")[0];
+const xbox = document.querySelectorAll("h1")[1];
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener("mouseenter", ()=>{
+    xbox.classList.add("newClass")
+})
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+left.addEventListener("mouseleave", ()=>{
+    xbox.classList.remove("newClass")
+})
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener("mouseenter", ()=>{
+    ps.classList.add("newClass")
+})
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+right.addEventListener("mouseleave", ()=>{
+    ps.classList.remove("newClass")
+})
